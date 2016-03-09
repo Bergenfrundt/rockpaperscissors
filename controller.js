23758b608a08be06d49a63435c2a59e17d1d3044
Loquacious <Loquacious@users.noreply.github.com>
2016-03-09 18:30:01 -0500

    Fix button

--- controller.js
+++ controller.js
@@ -1,6 +1,9 @@
-//This makes the play button work. Taken from arsh-sim
-window.onload = function() {
-    document.getElementById("play").onclick = function() {
+// This makes the play button work. Taken from arsh-sim.
+window.onload = function () {
+	var play = document.getElementById('play');
+	play.title = '';
+    play.onclick = function () {
         rps();
+		return false;
     };
 };
